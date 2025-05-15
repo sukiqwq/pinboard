61c8d09435ded68357d90f858cc9694d3154a797
suki <suki@suki-desktop.2ebz10BG0k>
2025-05-15 16:47:59 -0400

    remove "Save"button on pin item

--- pinboard-frontend/src/components/pin/PinItem.js
+++ pinboard-frontend/src/components/pin/PinItem.js
@@ -218,23 +218,6 @@ const PinItem = ({ pin }) => {
                 </svg>
                 {likeCount > 0 && <span style={{ marginLeft: '0.25rem' }}>{likeCount}</span>}
               </PinLikeButton>
-
-              {currentUser && (
-                <PinButton
-                  primary
-                  onClick={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    console.log('保存 Pin:', pin.pin_id);
-                  }}
-                  aria-label="保存图钉"
-                >
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2zm0 15l-5-2.18L7 18V5h10v13z" fill="currentColor" />
-                  </svg>
-                  保存
-                </PinButton>
-              )}
             </PinActions>
           </PinOverlayContent>
         </PinOverlay>
